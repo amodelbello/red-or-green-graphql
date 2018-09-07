8d9fc8d91da9c7c4da3a14d231de2237b273e191
Amo DelBello <amodelbello@gmail.com>
2018-09-07 16:16:53 -0600

    Now using graphql express server, and wraps start functionality in an async function.

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const expressPlayground = require('graphql-playground-middleware-express').default;
 const { merge } = require('lodash');
 
 const Business = require('./types/Business');
 const Category = require('./types/Category');
-
-const rootQuery = require('./types/rootQuery');
+const rootQuery = require('./types/Query');
 const resolvers = require('./resolvers');
 
+async function start() {
+  const app = express();
 
-// There's a bug in apollo playground. This needs to be set manually for now
-const playground = {
-  settings: {
-    'editor.cursorShape': 'line',
-  },
-};
+  // There's a bug in apollo playground. This needs to be set manually for now
+  const playground = {
+    settings: {
+      'editor.cursorShape': 'line',
+    },
+  };
 
-const server = new ApolloServer({ 
-  typeDefs: [
-    rootQuery,
-    Business,
-    Category,
-  ],
-  resolvers,
-  playground 
-});
+  const server = new ApolloServer({ 
+    typeDefs: [
+      rootQuery,
+      Business,
+      Category,
+    ],
+    resolvers,
+    playground 
+  });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+  server.applyMiddleware({ app });
+
+  app.get('/', (req, res) => res.end('Red Or Green GraphQL API'));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  app.listen({ port: 4000 }, () => {
+    console.log(`🚀 Server ready at  http://localhost:4000${server.graphqlPath}`);
+  });
+}
+
+start();
