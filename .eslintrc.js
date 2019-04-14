ed8f35d3d808b578c146ad331580eeae7189905e
Amo DelBello <amodelbello@gmail.com>
2019-04-14 10:09:41 -0600

    Adds eslint + prettier integration

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+  },
+};
